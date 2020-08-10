962c10a8554f20862bda7eef6a2850b5180e3ba6
wuyaheng <wuyaheng2016@gmail.com>
2020-08-10 16:56:21 -0400

    create getSampleText function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getSampleText()
+  }
+
+
+  getSampleText() {
+    axios.get('http://hispterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+    .then((response) => {
+      this.setState({text: response.data.text}, function() {
+        console.log(this.state);
+      })
+    })
+  }
 
   render() {
     return(

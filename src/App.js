c283d3714213127e86ae4e6de996a65650aa2dc2
wuyaheng <wuyaheng2016@gmail.com>
2020-08-12 09:05:33 -0400

    remove focus outline

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
   render() {
     return(
       <div className="App container">
-      <h1 className="text-center">Text Generator</h1>
+      <h1 className="text-center mt-3">Text Generator</h1>
       <hr/>
       <form className="form-inline">
       <div className="form-group">
